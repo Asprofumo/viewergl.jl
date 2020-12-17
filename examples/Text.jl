32633f6d047c8eeb8cba28a58b0a6490bb5cccff
simochilo <sim.chilosi@stud.uniroma3.it>
2020-12-17 16:39:03 +0100

    first

---
+++ examples/Text.jl
@@ -0,0 +1,7 @@
+using ViewerGL
+GL = ViewerGL
+
+GL.VIEW([
+	GL.GLFrame2,
+ 	GL.GLText("PLaSM", GL.COLORS[1])
+]);
---
+++ examples/allexamples.jl
@@ -0,0 +1,12 @@
+function allexamples()
+    include("Convex.jl")
+    include("Convex2d.jl")
+    include("Grid.jl")
+    include("Mesh.jl")
+    include("numbering.jl")
+    include("Polygon.jl")
+    include("Polyhedron.jl")
+    include("Text.jl")
+end
+
+allexamples()
